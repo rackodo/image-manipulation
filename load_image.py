340dad2232e0c8444d8a814e6cabae0004198b54
Bash Elliott <bashelliott@gmail.com>
2026-09-08 00:22:04 +1000

    INIT: + load image + sort columns of image via rgb channel + gradually sort image over time

---
+++ load_image.py
@@ -0,0 +1,37 @@
+from PIL import Image
+
+# make callable class
+class ImageRender:
+	def __init__(self):
+		self.im = Image.open( 'bash.jpg' ) # source image
+		self.wsize, self.ysize = self.im.size
+
+		self.offset = 0 # the offset according to image width
+		self.chunks = 10 # how many columns should be sorted at any given time, before the image is returned
+
+	# apply a pixel sort to the image up to a set column according to self.chunks, then return
+	# this allows for iterative sorting that can be retrieved over time
+	def updateImage(self):
+		for _ in range(self.chunks):
+			if self.offset >= self.wsize:
+				return self.im
+
+			# get a single column of pixels and turn them into rgba values
+			inCol = self.im.crop((self.offset, 0, self.offset + 1, self.ysize))
+			inPix = list(inCol.getdata())
+
+			# sort according to an rgba value. r = p[0], g = p[1], b = p[2], a = p[3]
+			inSort = sorted(inPix, key=lambda p: p[0])
+
+			# create a temporary image for the sorted column, then place the sorted pixels
+			outCol = Image.new(inCol.mode, (1, self.ysize))
+			outCol.putdata(inSort)
+
+			# paste the sorted portion over the original image
+			self.im.paste(outCol, (self.offset, 0))
+			self.offset += 1
+
+		return self.im
+
+
+
---
+++ view_image.py
@@ -0,0 +1,62 @@
+import pygame
+
+from load_image import ImageRender
+
+class Viewer:
+	def __init__(self, w: int, h: int):
+		self.w = w
+		self.h = h
+
+		# pygame boilerplate
+		pygame.init()
+		self.screen = pygame.display.set_mode((self.w, self.h))
+		pygame.display.set_caption("Shit fuck")
+		self.clock = pygame.time.Clock()
+
+		# call our image render class. it's called this until i think of a better name
+		self.render = ImageRender()
+
+	# start the thingy
+	def start(self):
+		self.paintPillowImage() # get the initial painted, edited image
+		running = True
+		while running:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					running = False
+
+			self.screen.fill((0, 0, 0))
+			self.paintPillowImage()
+			self.clock.tick(60)
+
+		pygame.quit()
+
+	def getPillowImage(self):
+		# get the updated, partially sorted image from the renderer
+		return self.render.updateImage()
+
+	def processPillowImage(self, pil_img):
+		pil_img = pil_img.convert("RGB")
+		pil_img.thumbnail((self.w, self.h))
+		# turn the Pillow image into a Pygame Surface
+		imgBytes = pil_img.tobytes()
+		imgSize = pil_img.size
+		imgMode = pil_img.mode
+
+		# fit the image into the window.
+
+		pygSurface = pygame.image.frombytes(imgBytes, imgSize, "RGB")
+
+		if imgMode == 'RGBA':
+			return pygSurface.convert_alpha()
+		return pygSurface.convert()
+
+	def paintPillowImage(self):
+		pil_img = self.getPillowImage() # get the image from pillow
+		surface = self.processPillowImage(pil_img) # get the surface from the processed pillow
+
+		self.screen.blit(surface, (0, 0))
+		pygame.display.flip()
+
+view = Viewer(500, 500)
+view.start()
